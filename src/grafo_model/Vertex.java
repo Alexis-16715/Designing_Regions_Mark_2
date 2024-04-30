a1e1ea2b8125337048c6d3bbd2a52b0487088a0c
Alexis-16715 <alexis.knack@hotmail.com>
2024-04-30 15:21:33 -0300

    V3

--- src/grafo_model/Vertex.java
+++ src/grafo_model/Vertex.java
@@ -1,5 +1,33 @@
 package grafo_model;
 
+import java.util.ArrayList;
+
 public class Vertex {
+
+    private String data;
+
+    private ArrayList<Edge> edges;
+
+
+    public Vertex(String data){
+        this.data = data;
+        this.edges = new ArrayList<Edge>();
+
+    }
+
+
+    public void addEdge(Vertex dest, Integer weight){
+        this.edges.add(new Edge(this, dest, weight));
+        // this.edges.add(new Edge(dest, this,weight));
+    }
+
+    public void removeEdge(Vertex dest){
+        edges.removeIf(edge -> edge.getDest().equals(dest));
+    }
+
+    public String getData() {
+        return data;
+    }
+
 
 }
